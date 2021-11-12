1f277f344b2f228418c16d2f993d2625f5dd1d68
WilliamDenega <williamdenega@gmail.com>
2021-11-12 18:03:11 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF3713";
+
+dodger.style.bottom = "0px";
+
+
+document.addEventListener("keydown", function(e){
+    if (e.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }
+    else if(e.key =="ArrowRight"){
+        moveDodgerRight();
+    }
+});
+
+
+
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers,10);
+
+    if (left > 0) {
+        dodger.style.left = `${left-1}px`;
+    }
+
+
+}
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px","")
+    const left = parseInt(leftNumbers,10);
+
+    if (left < 360){
+        dodger.style.left = `${left+1}px`
+    }
+
+}
